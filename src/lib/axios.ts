0da29c6f517eec3b9b2e9d03d719aec40250d787
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-05-01 17:30:41 -0300

    feat: adds react-query an axios

---
+++ src/lib/axios.ts
@@ -0,0 +1,31 @@
+'use server'
+
+import axios from 'axios'
+import { cookies } from 'next/headers'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  FINANCE_API_URL: z.string(),
+  FINANCE_APP_TOKEN: z.string(),
+})
+
+const env = envSchema.parse(process.env)
+
+export const api = axios.create({
+  baseURL: env.FINANCE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    isStaticToken: false,
+  },
+})
+
+api.interceptors.request.use((config) => {
+  if (config.headers.isStaticToken) {
+    config.headers.Authorization = `Bearer ${env.FINANCE_APP_TOKEN}`
+  } else {
+    const token = cookies().get('finance-token')
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
