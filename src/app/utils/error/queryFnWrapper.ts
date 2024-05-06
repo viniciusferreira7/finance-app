e2185e2ab8784baf6bf761b3e9d96fb1d6636d8c
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-05-06 14:53:21 -0300

    feat: creates a function to wrap a request

---
+++ src/app/utils/error/queryFnWrapper.ts
@@ -0,0 +1,16 @@
+import { ErrorServerAction } from '@/models/error'
+
+export async function queryFnWrapper<T, K>(
+  fn: (args: K) => Promise<T | ErrorServerAction>,
+  args: K,
+): Promise<T> {
+  const data = await fn(args)
+
+  if (data && typeof data === 'object' && 'message' in data) {
+    const { message } = data
+
+    throw new Error(message)
+  }
+
+  return data as T
+}
