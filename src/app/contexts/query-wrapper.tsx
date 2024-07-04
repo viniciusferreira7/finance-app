79c10a038a21370662a7b174560dc89c9812fd7b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-07-04 09:45:59 -0300

    chore: adds the dev tools for tankstack

--- src/app/contexts/query-wrapper.tsx
+++ src/app/contexts/query-wrapper.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactNode } from 'react'
 
 export function QueryWrapper({ children }: { children: ReactNode }) {
   const queryClient = new QueryClient()
 
   return (
-    <QueryClientProvider client={queryClient}>{children} </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
