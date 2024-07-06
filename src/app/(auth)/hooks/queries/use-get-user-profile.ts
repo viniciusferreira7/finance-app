6e31eec240da12ff87557d3e47bce317ea423cb7
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-07-06 14:03:01 -0300

    fix: adjust the get user profile hook

--- src/app/(auth)/hooks/queries/use-get-user-profile.ts
+++ src/app/(auth)/hooks/queries/use-get-user-profile.ts
@@ -1,15 +1,22 @@
 'use client'
 
 import { useQuery } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 
 import { getUserProfile } from '@/services/user'
 import { queryFnWrapper } from '@/utils/error/query-fn-wrapper'
 
 export const useGetUserProfile = () => {
+  const router = useRouter()
+
   const query = useQuery({
     queryKey: ['get-user-profile'],
     queryFn: async () => queryFnWrapper(getUserProfile, {}),
   })
+
+  if (query.isError) {
+    router.push('/sign-in')
+  }
 
   return query
 }
