e9e4871603a4b40cdcc170eb81cdf78fa4908d00
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-07-18 08:40:36 -0300

    fix: adjusts the function of get the object's query keys

--- src/services/incomes/fetch-incomes.ts
+++ src/services/incomes/fetch-incomes.ts
@@ -11,6 +11,18 @@ export interface FetchIncomesParams {
     page?: number
     per_page?: number
     pagination_disabled?: boolean
+    name?: string | null
+    value?: string | null
+    createdAt?: {
+      from: string | null
+      to: string | null
+    }
+    updatedAt?: {
+      from: string | null
+      to: string | null
+    }
+    category?: string | null
+    sort?: string | null
   }
 }
 
--- src/utils/search-params/get-query-keys.ts
+++ src/utils/search-params/get-query-keys.ts
@@ -4,7 +4,28 @@ export function getQueryKeys<T extends object>(
   if (queries) {
     const transformIntoArray = Object.entries(queries)
 
-    const flatArray = transformIntoArray.flatMap((query) => query)
+    const flatArray = transformIntoArray.flatMap((query) => {
+      const [fieldName, value] = query
+
+      if (value) {
+        if (typeof value === 'object') {
+          const subQuery = Object.entries(value).map((item) => {
+            const [fieldItem, itemValue] = item
+
+            if (itemValue) {
+              return [`${fieldName}-${fieldItem}` ,itemValue ]
+            }
+
+            return []
+          }).flatMap(item => item)
+
+          return subQuery
+        }
+        return query
+      }
+
+      return []
+    })
 
     return flatArray
   }
