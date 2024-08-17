bfb0850fdc53142587e0ae470e689196a5fc0d89
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-08-17 13:53:44 -0300

    test: creates test E2E to create an expense

--- test/create-expense.e2e.spec.ts
+++ test/create-expense.e2e.spec.ts
@@ -3,22 +3,22 @@ import { expect, test } from '@playwright/test'
 
 import { signIn } from './utils/sign-in'
 
-test('should be able to create an income', async ({ page }) => {
+test('should be able to create an expense', async ({ page }) => {
   await signIn({ page })
 
   await page.getByRole('button', { name: 'Create' }).click()
   await page.waitForLoadState('networkidle')
 
-  await page.getByRole('button', { name: 'Income' }).click()
+  await page.getByRole('button', { name: 'Expense' }).click()
   await page.waitForLoadState('networkidle')
 
-  const incomeName = faker.finance.accountName()
+  const expenseName = faker.commerce.productName()
 
-  await page.getByLabel('Name').fill(incomeName)
+  await page.getByLabel('Name').fill(expenseName)
 
-  const incomeValue = Number(faker.finance.amount({ min: 10, max: 1_000_000 }))
+  const expenseValue = Number(faker.commerce.price({ min: 10, max: 1_000_000 }))
 
-  await page.getByLabel('Value').fill(incomeValue.toString())
+  await page.getByLabel('Value').fill(expenseValue.toString())
 
   await page.getByText('...').click()
   await page.waitForLoadState('networkidle')
@@ -30,17 +30,17 @@ test('should be able to create an income', async ({ page }) => {
   await page.getByRole('option', { name: 'a' }).first().click()
   await page.waitForLoadState('networkidle')
 
-  const incomeDescription = faker.helpers.arrayElement([
+  const expenseDescription = faker.helpers.arrayElement([
     faker.lorem.words({ min: 10, max: 40 }).slice(0, 220),
     faker.lorem.words({ min: 50, max: 100 }).slice(0, 220),
   ])
 
-  await page.locator('textarea[name="description"]').fill(incomeDescription)
+  await page.locator('textarea[name="description"]').fill(expenseDescription)
 
   await page.getByRole('button', { name: 'Create' }).click()
   await page.waitForLoadState('networkidle')
 
-  const toast = page.getByText('Income was created successfully.')
+  const toast = page.getByText('Expense was created successfully.')
 
   await expect(toast).toBeVisible()
 })
