4f8cdb987a6ad9acb3e43e4ace7102536c76fac0
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-09-16 09:59:19 -0300

    fix: adds the exchange rate inside the convert to currency function

--- src/utils/currency/format-currency.ts
+++ src/utils/currency/format-currency.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import { convertToCurrency } from './convert-to-currency'
 
 interface FormatCurrency {
@@ -9,15 +11,20 @@ export function formatCurrency(
   { isToConvertToCurrency = true }: FormatCurrency = {},
 ) {
   let numValue = parseFloat(value.toString())
+  const exchangeRates = JSON.parse(
+    localStorage.getItem('finance-exchange-rates') ?? '',
+  )
+
+  const [[currency, currencyValue]] = Object.entries(exchangeRates ?? {})
 
   if (isToConvertToCurrency) {
-    const currency = convertToCurrency(value)
+    const currency = convertToCurrency(value * Number(currencyValue))
     numValue = currency
   }
 
   return numValue.toLocaleString('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency,
     maximumFractionDigits: 2,
   })
 }
